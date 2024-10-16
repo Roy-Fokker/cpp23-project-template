a2f7474c305812f7bd65eb079aabed2558a7e07a
Roy-Fokker <11281115+Roy-Fokker@users.noreply.github.com>
2024-10-15 20:24:09 -0400

    It surprisingly works on 1st try!!!

---
+++ cmake/cmake-3.30-stdmods.cmake
@@ -0,0 +1,11 @@
+# Solution is experimental in CMake 3.30
+cmake_minimum_required(VERSION 3.30 FATAL_ERROR)
+
+# Set the experimental flag
+set(CMAKE_EXPERIMENTAL_CXX_IMPORT_STD
+	"0e5b6991-d74f-4b3d-a41c-cf096e0b2508" # current value as per https://www.kitware.com/import-std-in-cmake-3-30/
+)
+
+# tell CMake we want to use 'import std' 
+# will get enabled for all targets declared after this
+set(CMAKE_CXX_MODULE_STD 1)
